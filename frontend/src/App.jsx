c04400468dd751b4738bcb682af29928c3965db7
DESHANISIRIWARDHANA <Ayeshadesh0319@gmail.com>
2025-02-06 15:24:27 +0530

    home page created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
-import Navbar from './components/Navbar';
+// import Navbar from './components/Navbar';
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
-      <Navbar />
+      {/* <Navbar /> */}
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
