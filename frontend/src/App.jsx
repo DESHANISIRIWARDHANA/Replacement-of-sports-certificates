d759057bd8d35861866e4e9e0e888d850a9498fa
sabaragamuwaSATD <125716628+DESHANISIRIWARDHANA@users.noreply.github.com>
2025-02-21 17:34:28 +0530

    ui change

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -24,6 +24,7 @@ import PersonalDetails from "./components/PersonalDetails";
 import CertificateRequestForm from "../src/pages/requestForm/cerificate-request-form";
 
 import ApplyRequestCertificatePage from "./pages/ApplyRequestCertificatePage";
+import RequestView from "./pages/Admin/RequestViewPage";
 
 const volleyballImageUrl =
   "https://s3-alpha-sig.figma.com/img/7043/3437/f735a6d5371f7616d4e1ec2a38029456?Expires=1740355200&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=ob4LVnCtmW-ujP9ffpli13ESvh4AJLHA5SfxsTfPZgVy2AUnxhwbnDyR9NFnh0weKWH~RuJbvz~bjp6c4nkxH8cFWHUx1Hw119oBIFIvr7J3vGikigXJec7aahgphqPr3oZ9GrCAEaphu3vCFwRKKahO6LAczJFlsEL3PcIXcre2qu35yublajRp0ff58UfT7domG-MxFJPCZtFRo1dhLN0DLHvZhAtEisYbsjcC1XieUcyYdwivUH7ug9p3i~2QVNNibLYa9qLtwMEYFQ~aypaZfIK3scUzSE4MI~AsqbKstrClur8sDhESug0hv5LuFMuDr0bFlXLDgpxOpAwizw__";
@@ -65,6 +66,7 @@ function App() {
         <Route path="/student-details" element={<PersonalDetails />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/admin/certification" element={<CertificationDetails />} />
+        <Route path="/admin/requestView" element={<RequestView />} />
         <Route
           path="/apply-request-certificate"
           element={
